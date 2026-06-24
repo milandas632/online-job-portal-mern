fb06304ba63a32f23b41c5f6b6f35ad9957c26cf
milandas632 <milandas632@gmail.com>
2026-06-24 12:41:32 +0530

    Add files via upload

--- server/src/server.js
+++ server/src/server.js
@@ -1,35 +1,14 @@
 import app from './app.js';
 import { connectDatabase } from './config/db.js';
 import { env } from './config/env.js';
-import Category from './models/Category.js';
-
-const defaultCategories = [
-  'Software Development',
-  'Data & Analytics',
-  'Design',
-  'Marketing',
-  'Finance',
-  'Human Resources'
-];
-
-async function ensureDefaultCategories() {
-  for (const name of defaultCategories) {
-    const slug = name
-      .toLowerCase()
-      .replace(/[^a-z0-9]+/g, '-')
-      .replace(/^-|-$/g, '');
-
-    await Category.findOneAndUpdate(
-      { slug },
-      { $setOnInsert: { name, slug } },
-      { upsert: true, new: true }
-    );
-  }
-}
+import { populateDemoData } from './populateDemoData.js';
 
 async function start() {
   await connectDatabase();
-  await ensureDefaultCategories();
+
+  if (String(process.env.POPULATE_DEMO_DATA || 'false') === 'true') {
+    await populateDemoData();
+  }
 
   app.listen(env.port, '0.0.0.0', () => {
     console.log(`Server listening on http://0.0.0.0:${env.port}`);
